046e40d035289040852c03769099e60b2b32436e
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-03-07 20:24:34 +0530

    impolemented withdraw function in fundme app

--- vanillajs-dapp-01/index.js
+++ vanillajs-dapp-01/index.js
@@ -10,6 +10,7 @@ const balanceButton = document.getElementById('balanceButton')
 connectButton.onclick = connect
 withdrawButton.onclick = withdraw
 fundButton.onclick = fund
+balanceButton.onclick = getBalance
 
 async function connect() {
   if (typeof window.ethereum !== 'undefined') {
@@ -26,18 +27,33 @@ async function connect() {
   }
 }
 
+async function getBalance() {
+  if (typeof window.ethereum !== 'undefined') {
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const balance = await provider.getBalance(contractAddress)
+    console.log(
+      'the current account balance is : ',
+      ethers.formatEther(balance)
+    )
+  }
+}
+
 async function fund() {
   console.log(`funding...`)
-  const ethAmount = '77'
+  const ethAmount = document.getElementById('ethAmount').value
   if (typeof window.ethereum !== 'undefined') {
     const provider = new ethers.BrowserProvider(window.ethereum) //browser provider instead of ethers.provider.web3provider
     const signer = await provider.getSigner()
     console.log(signer)
     const contract = new ethers.Contract(contractAddress, abi, signer)
     try {
+      console.log('funding with : ', ethAmount)
       const transactionResponse = await contract.fund({
         value: ethers.parseEther(ethAmount),
       })
+      // how to give indication the transaction has run throug and succeeded.
+      // listen for tx to be mined.
+      await listenForTransactionMine(transactionResponse, provider)
     } catch (error) {
       console.log(error)
     }
@@ -59,5 +75,31 @@ async function withdraw() {
     })
   } else {
     // connect.log('maa chudaye')
+  }
+}
+
+// function listenForTransactionMine(transactionResponse, provider) {
+//   console.log('mining tansaction: ', transactionResponse.hash)
+//   provider.once(transactionResponse.hash, (transactionReceipt) => {
+//     console.log(
+//       'completed with tranasacion :  ',
+//       transactionReceipt.confimations
+//     )
+//   })
+// }  //this provider.once had becomce outdated
+
+async function listenForTransactionMine(transactionResponse, provider) {
+  console.log('Mining transaction:', transactionResponse.hash)
+
+  try {
+    const transactionReceipt = await provider.getTransactionReceipt(
+      transactionResponse.hash
+    )
+    console.log(
+      'Completed with confirmations: ',
+      transactionReceipt.blockNumber
+    )
+  } catch (error) {
+    console.error('Error while listening for transaction mining:', error)
   }
 }
