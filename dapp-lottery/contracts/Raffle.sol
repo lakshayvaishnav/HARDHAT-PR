6b856e1e961b2db7d372800afafce59c2ede8f44
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-03-09 00:28:47 +0530

    finding random winner logic implementation

--- dapp-lottery/contracts/Raffle.sol
+++ dapp-lottery/contracts/Raffle.sol
@@ -11,19 +11,25 @@ contract Raffle is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
     // Events
     event RaffleEnter(address indexed players);
+    event RequestRaffleWinner(uint256 indexed requestId);
 
     constructor(
         address vrfCoordinatorv2,
         uint256 enteranceFee,
         bytes32 gasLane,
-        uint64 subscriptionId
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorv2) {
         i_enteranceFee = enteranceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorv2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -33,19 +39,22 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function requestRandomWinner() external {
-        i_vrfCoordinator.requestRandomWords(
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
+            REQUEST_CONFIRMATION,
+            i_callbackGasLimit,
+            NUM_WORDS
         );
+        emit RequestRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(
         uint256 requrestId,
         uint256[] memory randomWords
-    ) internal override {}
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinners = s_players[indexOfWinner]}
 
     function getEnteranceFee() public view returns (uint256) {
         return i_enteranceFee;
