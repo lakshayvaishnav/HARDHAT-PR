b043595bed9b2f4f126ad5ebc8d68283b51508e0
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-03-22 00:55:41 +0530

    added deploy scripts

--- hardhat-erc20/contracts/OurToken.sol
+++ hardhat-erc20/contracts/OurToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
-    
-    constructor (uint256 inititalSupply) ERC20("OurToken","OT")
-    _mint(msg.sender,  inititalSupply)
+    constructor(uint256 inititalSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, inititalSupply);
+    }
 }
--- hardhat-erc20/hardhat.config.js
+++ hardhat-erc20/hardhat.config.js
@@ -1,4 +1,66 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
 module.exports = {
-  solidity: "0.8.20",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // },
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //accounts: {
+      //     mnemonic: MNEMONIC,
+      // },
+      saveDeployments: true,
+      chainId: 11155111,
+      blockConfirmation: 6,
+    },
+  },
+  etherscan: {
+    // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+      // polygon: POLYGONSCAN_API_KEY,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    user1: {
+      default: 1,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.8.20",
+      },
+    ],
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
 };
