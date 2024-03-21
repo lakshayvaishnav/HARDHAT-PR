add05006e918266d85ecefde4ab1e384393710f9
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-03-22 03:42:58 +0530

    wrote scripts to getweth

--- defi-aave/scripts/getWeth.js
+++ defi-aave/scripts/getWeth.js
@@ -1,9 +1,21 @@
-const { getNamedAccounts } = require("hardhat")
+const { getNamedAccounts, ethers } = require("hardhat")
 
+const AMOUNT = ethers.parseEther("0.02")
 async function getweth() {
     const { deployer } = getNamedAccounts()
     // call the "deposit" function on the weth contract
-    // abi , address.
+    // abi , address
+
+    const iweth = await ethers.getContractAt(
+        "IWeth",
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        deployer
+    )
+
+    const tx = await iweth.deposit({ value: AMOUNT })
+    await tx.wait(1)
+    const wethBalance = await iweth.balanceOf(deployer)
+    console.log(`Got ${wethBalance.toString()} this much weth ...`)
 }
 
 module.exports = {
